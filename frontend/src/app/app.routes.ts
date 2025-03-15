293efdad23a325eef76ad0bd7f30678fbe7abd91
Joel Erulu <joelerulu088@gmail.com>
2025-03-15 11:21:08 -0400

    Setup Frontend and Backend

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { VinDetailsComponent } from './components/vin-details/vin-details.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'vinDetails', component: VinDetailsComponent },
+
+];
---
+++ frontend/src/app/components/vin-details/vin-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vin-details',
+  standalone: true,
+
+  imports: [],
+  templateUrl: './vin-details.component.html',
+  styleUrl: './vin-details.component.css'
+})
+export class VinDetailsComponent {
+
+}
